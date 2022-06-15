dcb740f73c0ff2417e4b7831ec6c94ae9be7bdcf
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2022-06-15 00:27:11 -0300

    Feat: get posts list from server

--- src/components/pages/TimelinePage/index.jsx
+++ src/components/pages/TimelinePage/index.jsx
@@ -1,9 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
+import PostsList from "../../PostsList";
+
 export default function TimelinePage() {
+    const [posts, setPosts] = useState(null);
+
+    useEffect(() => {
+        axios.get('http://localhost:4000/posts')
+        .then(response => {
+            setPosts(response.data);
+        })
+        .catch(err => console.log(err));
+    }, []);
+
+    const postsList = posts ? <PostsList posts={posts}></PostsList> : undefined;
+
     return (
         <TimelineContainer>
-            
+            {postsList}
         </TimelineContainer>
     )
 }
