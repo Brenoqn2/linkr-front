789fd3e9772143dfb97e42eba237a47735c5fa2f
i-Lucas <novo.contato.lucas@gmail.com>
2022-06-16 04:42:20 -0300

    decreasing maximum image size to avoid 413

--- src/components/pages/SignupPage/index.jsx
+++ src/components/pages/SignupPage/index.jsx
@@ -89,7 +89,7 @@ function GetAvatar(data, setData) {
     const file = document.getElementById('file').files[0];
     const reader = new FileReader();
     reader.readAsDataURL(file);
-    if (file.size > max_size) return alert('File is too big!');
+    if (file.size > max_size) return alert('File is too big!\nMax size is 70kb');
     reader.onload = function () {
         setData({ ...data, avatar: reader.result });
     };
