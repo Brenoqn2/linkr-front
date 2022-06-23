277c53b2940570e49df06cae33e528fc9da06066
Alexandre Costa <alexandreodpc@gmail.com>
2022-06-23 20:59:41 -0300

    merge

--- src/components/PostsList/index.jsx
+++ src/components/PostsList/index.jsx
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 
 import Post from "../Post/";
-// import Repost from "../Repost";
+import Repost from "../Repost";
 
 export default function PostsList({ posts }) {
-  const content = posts.map((post) => <Post data={post} key={post.id}></Post>);
+  const content = posts.map((post) => {
+    if (post.postId === 0) return <Post data={post} key={post.id}></Post>;
+    else return <Repost data={post} key={post.id}></Repost>;
+  });
 
   return <Posts>{content}</Posts>;
 }
