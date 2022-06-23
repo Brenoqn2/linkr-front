97b8e8ca013c6f1e0234cba2173201d99fa94d0d
Brenoqn2 <brenoqn2@gmail.com>
2022-06-23 14:29:14 -0300

    feat: hashtagpage infinite scroll

--- src/components/pages/HashtagPage/index.jsx
+++ src/components/pages/HashtagPage/index.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState, useContext, useCallback } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { ThreeDots } from "react-loader-spinner";
 import styled from "styled-components";
+import InfiniteScroll from "react-infinite-scroller";
 
 import UserContext from "../../../contexts/userContext";
 import TokenContext from "../../../contexts/tokenContext";
@@ -15,10 +16,13 @@ import GetTokenAndHeaders from "../../Resources/GetTokenAndHeaders";
 import config from "../../../config/config.json";
 
 export default function HashtagPage() {
-  const API = config.API;
+  const API = config.API_LOCAL;
   const navigate = useNavigate();
   const [posts, setPosts] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [hasNextPage, setHasNextPage] = useState(true);
+  const [loadingNewPosts, setLoadingNewPosts] = useState(false);
+  const [page, setPage] = useState(1);
 
   const { userData } = useContext(UserContext);
   const { setToken } = useContext(TokenContext);
@@ -44,12 +48,48 @@ export default function HashtagPage() {
     console.log("useeffect do hashtagpage");
     if (posts === null) {
       setLoading(true);
+      console.log("getposts");
       getPosts();
     }
   }, [getPosts, posts]);
 
+  function getMorePosts(page) {
+    console.log("PAGINA", page);
+    setLoadingNewPosts(true);
+    axios
+      .get(`${API}/hashtag/${hashtag.toLowerCase()}?page=${page + 1}`, header)
+      .then((response) => {
+        console.log("aqui", response.data);
+        setPosts([...posts, ...response.data]);
+        setPage(page + 1);
+        checkPosts();
+        setLoadingNewPosts(false);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function checkPosts() {
+    axios
+      .get(`${API}/checkPosts?page=${page + 2}`, header)
+      .then((response) => {
+        setHasNextPage(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        return false;
+      });
+  }
+
   const postsList = posts?.length ? (
-    <PostsList posts={posts}></PostsList>
+    <InfiniteScroll
+      data-testid="episodes-infinite-scroll"
+      pageStart={0}
+      loadMore={getMorePosts}
+      hasMore={hasNextPage && !loadingNewPosts}
+      loader={<ThreeDots width={30} height={10} color={"#fff"} />}
+    >
+      <PostsList posts={posts}></PostsList>
+    </InfiniteScroll>
   ) : (
     <NoContent>There are no posts yet</NoContent>
   );
